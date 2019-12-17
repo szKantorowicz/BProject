f33dcd336d010552a39d8436ab540cd308e16b8c
szkantorowicz <szkantorowicz@gmail.com>
2019-12-17 22:09:58 +0100

    Poprawka z Consol na Console

--- src/BProject/BProject.Console/Properties/AssemblyInfo.cs
+++ src/BProject/BProject.Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("BProject.Consol")]
+[assembly: AssemblyTitle("BProject.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BProject.Consol")]
+[assembly: AssemblyProduct("BProject.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
