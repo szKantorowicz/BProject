2eb4499f438287726e2b1ac7e84417b5bc7495d4
szkantorowicz <szkantorowicz@gmail.com>
2019-12-19 02:44:24 +0100

    Skonczony ProductConfiguration

--- src/BProject/BProject.Core/EntityTypeConfiguration/ProductConfiguration.cs
+++ src/BProject/BProject.Core/EntityTypeConfiguration/ProductConfiguration.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Bproject.Core.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BProject.Core.EntityTypeConfiguration
 {
-   public  class ProductConfiguration
+    public class ProductConfiguration : EntityTypeConfiguration<Product>
     {
-    }
-}
+        public ProductConfiguration()
+        {
+            HasKey(p => p.ID);
+
+            Property(p => p.ID)
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                .IsRequired();
+
+            Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            Property(p => p.Description)
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            Property(p => p.Price)
+                .IsRequired();
+
+            Property(p => p.Quantityinstock)
+                .IsRequired();
+
+            Property(p => p.Avilability)
+                .IsRequired();
+
+            Property(p => p.CreatedDate)
+                .IsRequired();
+
+            Property(p => p.UpdatedDate)
+                .IsOptional();
+
+        }
+
+    }  }       
