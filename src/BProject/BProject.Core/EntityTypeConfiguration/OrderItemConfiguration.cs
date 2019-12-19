b45de8717ab893a13f5c65b4f3efe26f770db648
szkantorowicz <szkantorowicz@gmail.com>
2019-12-19 02:19:12 +0100

    Skończony OrderItemConfiguration

--- src/BProject/BProject.Core/EntityTypeConfiguration/OrderItemConfiguration.cs
+++ src/BProject/BProject.Core/EntityTypeConfiguration/OrderItemConfiguration.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Bproject.Core.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BProject.Core.EntityTypeConfiguration
 {
-    class OrderItemConfiguration
+    public class OrderItemConfiguration : EntityTypeConfiguration<OrderItem>
     {
+        public OrderItemConfiguration()
+        {
+            HasKey(oi => oi.ID);
+
+            Property(oi => oi.ID)
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                .IsRequired();
+
+            Property(oi => oi.CreatedDate)
+                .IsRequired();
+
+            Property(oi => oi.UpdatedDate)
+                .IsOptional();
+
+            Property(oi => oi.Quantity)
+                .IsRequired();
+
+            Property(oi => oi.TotalPrice)
+                .IsRequired();
+
+            Property(oi => oi.UnitPrice)
+                .IsRequired();
+
+            Property(oi => oi.ProductID)
+                .IsRequired();
+
+            Property(oi => oi.OrderID)
+                .IsRequired();
+
+            
+
+
+
+
+        }
     }
 }
