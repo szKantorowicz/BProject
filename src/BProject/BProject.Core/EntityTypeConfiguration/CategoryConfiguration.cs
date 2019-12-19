a7a9d9b93ec6f16bc081fb150f5d76ee1df62518
szkantorowicz <szkantorowicz@gmail.com>
2019-12-19 02:49:02 +0100

    Skończony Category Configuration

--- src/BProject/BProject.Core/EntityTypeConfiguration/CategoryConfiguration.cs
+++ src/BProject/BProject.Core/EntityTypeConfiguration/CategoryConfiguration.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Bproject.Core.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BProject.Core.EntityTypeConfiguration
 {
-    class CategoryConfiguration
-    {
-    }
+        class CategoryConfiguration : EntityTypeConfiguration<Category>
+        {
+            public CategoryConfiguration()
+            {
+                HasKey(ca => ca.ID);
+
+                Property(ca => ca.ID)
+                    .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                    .IsRequired();
+
+                Property(ca => ca.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+                Property(ca => ca.Description)
+                    .HasMaxLength(1000)
+                    .IsRequired();
+        }
 }
