79a0740aeb7267250ee94b67dbac3d7a5bd4042a
szkantorowicz <szkantorowicz@gmail.com>
2019-12-20 22:28:01 +0100

    Skończenie OrderItem w SQL

--- database_scripts/tabels/create_orderItem.sql
+++ database_scripts/tabels/create_orderItem.sql
@@ -1,14 +1,15 @@
 use customer_db
 
-create table dbo.OrderItem
-(
-ID int  NOT NULL PRIMARY KEY,
-OrderID int FOREIGN KEY dbo.Order,
-ProductID int FOREIGN KEY dbo.Product,
-UnitPrice decimal(10,2),
-Price int,
-TotalPrice decimal(10,2),
-Quantity int,
-UpdatedDate datetime,
-CreatedDate datetime
+CREATE TABLE [dbo].[OrderItems] (
+    [ID] [int] NOT NULL IDENTITY,
+    [OrderID] [int],
+    [ProductID] [int],
+    [UnitPrice] [decimal](18, 2),
+    [TotalPrice] [decimal](18, 2),
+    [Quantity] [int],
+    [UpdatedDate] [datetime],
+    [CreatedDate] [datetime],
+    CONSTRAINT [PK_dbo.OrderItems] PRIMARY KEY ([ID])
 )
+CREATE INDEX [IX_OrderID] ON [dbo].[OrderItems]([OrderID])
+CREATE INDEX [IX_ProductID] ON [dbo].[OrderItems]([ProductID])
