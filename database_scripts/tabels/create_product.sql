ab1fae63a4454e8f016371bdca473910f7a168f4
szkantorowicz <szkantorowicz@gmail.com>
2019-12-20 22:29:24 +0100

    Skończenie Products w SQL

--- database_scripts/tabels/create_product.sql
+++ database_scripts/tabels/create_product.sql
@@ -1,14 +1,14 @@
 use customer_db
 
-create table dbo.Product
-(
-ID int PRIMARY KEY FOREIGN KEY dbo.Product,
-CategoryId int FOREIGN KEY dbo.Category,
-Name nvarchar(50),
-Description nvarchar(max),
-Price decimal(10,2),
-QuantityInStock int,
-Availablity bit,
-UpdatedDate datetime,
-CreatedDate datetime
+CREATE TABLE [dbo].[Products] (
+    [ID] [int] NOT NULL IDENTITY,
+    [Category] [int],
+    [Name] [nvarchar](max),
+    [Description] [nvarchar](max),
+    [Price] [decimal](18, 2),
+    [Quantityinstock] [int],
+    [Avilability] [bit],
+    [UpdatedDate] [datetime],
+    [CreatedDate] [datetime],
+    CONSTRAINT [PK_dbo.Products] PRIMARY KEY ([ID])
 )
