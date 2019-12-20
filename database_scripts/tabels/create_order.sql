46482d13031a8475b5946cf255579eb8888c92e8
szkantorowicz <szkantorowicz@gmail.com>
2019-12-20 22:27:01 +0100

    Skończenie Orders w SQL

--- database_scripts/tabels/create_order.sql
+++ database_scripts/tabels/create_order.sql
@@ -1,15 +1,18 @@
 use customer_db
 
 
-create table dbo.[Order]
-(
-ID int  NOT NULL PRIMARY KEY 
-CustomerID int FOREIGN KEY dbo.Customer,
-TotalAmount int,
-PaymentType int,
-IsPayed bit, PaymentTypeID int FOREIGN KEY dbo.PaymentType
-[Status] int,
-StatusID int FOREIGN KEY dbo.Status,
-UpdatedDate datetime,
-CreatedDate datetime
+CREATE TABLE [dbo].[Orders] (
+    [ID] [int] NOT NULL IDENTITY,
+    [CustomerID] [int],
+    [TotalAmount] [decimal](18, 2),
+    [IsPayed] [bit],
+    [PaymentTypeID] [int],
+    [Status] [int],
+    [UpdatedDate] [datetime],
+    [CreatedDate] [datetime],
+    [Status1_ID] [int],
+    CONSTRAINT [PK_dbo.Orders] PRIMARY KEY ([ID])
 )
+CREATE INDEX [IX_CustomerID] ON [dbo].[Orders]([CustomerID])
+CREATE INDEX [IX_PaymentTypeID] ON [dbo].[Orders]([PaymentTypeID])
+CREATE INDEX [IX_Status1_ID] ON [dbo].[Orders]([Status1_ID])
