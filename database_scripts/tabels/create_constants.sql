0a53179371aeaf700718375c924eb2229ec50dec
szkantorowicz <szkantorowicz@gmail.com>
2019-12-20 22:37:55 +0100

    Dodanie Constantów w SQL

--- database_scripts/tabels/create_constants.sql
+++ database_scripts/tabels/create_constants.sql
@@ -1,26 +1,12 @@
 use customer_db
 
-alter table dbo.[UserRole] alter column UserID int NOT NULL 
-alter table dbo.[UserRole] alter column RoleID int NOT NULL
-alter table dbo.[UserRole] add constraint PK_User_Role primary key (UserID,RoleID) 
-alter table dbo.[UserRole] add constraint Fk_User foreign key (UserID) references dbo.[User] (ID) on delete cascade on update cascade
-alter table dbo.[UserRole] add constraint Fk_Role foreign key (RoleID) references dbo.[Role] (ID) on delete cascade on update cascade
-
-alter table dbo.Customer add constraint UQ_Customer_UserID UNIQUE (UserID)
-alter table dbo.[Customer] add constraint Fk_Customer_User foreign key (UserID) references dbo.[User] (ID) on delete cascade on update cascade
-
-alter table dbo.[Address] add constraint Fk_Address_Customer foreign key (CustomerID) references dbo.[Customer] (ID) on delete cascade on update cascade
-
-alter table dbo.[Order] add constraint Fk_Order_Customer foreign key (CustomerID) references dbo.[Customer] (ID) on delete cascade on update cascade
-
-alter table dbo.[ProductCategory] add constraint Fk_ProductCategory_Product foreign key ([ProductID]) references dbo.[Product] (ID) on delete cascade on update cascade
-
-alter table dbo.[ProductCategory] add constraint Fk_ProductCategory_Category foreign key ([CategoryID]) references dbo.[Category] (ID) on delete cascade on update cascade
-
-alter table dbo.[Order] add constraint Fk_Order_Status foreign key ([StatusID]) references dbo.[Status] (ID) on delete cascade on update cascade
-
-alter table dbo.[Order] add constraint Fk_Order_PaymentType foreign key ([PaymentTypeID]) references dbo.[PaymentType] (ID) on delete cascade on update cascade
-
-alter table dbo.OrderItem add constraint FK_OrderItem_Order foreign key (OrderID) references dbo.[Order] (ID) on delete cascade on update cascade
-
-alter table dbo.OrderItem add constraint FK_OrderItem_Product foreign key (ProductID) references dbo.[Product] (ID) on delete cascade on update cascade
+ALTER TABLE [dbo].[Addresses] ADD CONSTRAINT [FK_dbo.Addresses_dbo.Customers_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([ID])
+ALTER TABLE [dbo].[Customers] ADD CONSTRAINT [FK_dbo.Customers_dbo.Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([ID])
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_dbo.Orders_dbo.Customers_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([ID])
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_dbo.Orders_dbo.PaymentTypes_PaymentTypeID] FOREIGN KEY ([PaymentTypeID]) REFERENCES [dbo].[PaymentTypes] ([ID])
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_dbo.Orders_dbo.Status_Status1_ID] FOREIGN KEY ([Status1_ID]) REFERENCES [dbo].[Status] ([ID])
+ALTER TABLE [dbo].[OrderItems] ADD CONSTRAINT [FK_dbo.OrderItems_dbo.Orders_OrderID] FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Orders] ([ID])
+ALTER TABLE [dbo].[OrderItems] ADD CONSTRAINT [FK_dbo.OrderItems_dbo.Products_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ID])
+ALTER TABLE [dbo].[Users] ADD CONSTRAINT [FK_dbo.Users_dbo.Roles_Role_ID] FOREIGN KEY ([Role_ID]) REFERENCES [dbo].[Roles] ([ID])
+ALTER TABLE [dbo].[CategoryProducts] ADD CONSTRAINT [FK_dbo.CategoryProducts_dbo.Categories_Category_ID] FOREIGN KEY ([Category_ID]) REFERENCES [dbo].[Categories] ([ID]) ON DELETE CASCADE
+ALTER TABLE [dbo].[CategoryProducts] ADD CONSTRAINT [FK_dbo.CategoryProducts_dbo.Products_Product_ID] FOREIGN KEY ([Product_ID]) REFERENCES [dbo].[Products] ([ID]) ON DELETE CASCADE
