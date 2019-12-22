5d79f9a0573cc0ab9d3cc5e9bc55844e978d097d
szkantorowicz <szkantorowicz@gmail.com>
2019-12-22 22:40:53 +0100

    Skończenie Role

--- database_scripts/tabels/create_role.sql
+++ database_scripts/tabels/create_role.sql
@@ -1,10 +1,10 @@
-use customer_db
+use BProject_db
 
 CREATE TABLE [dbo].[Roles] 
 (
     [ID] [int] NOT NULL IDENTITY,
-    [Name] [nvarchar](max),
+    [Name] [nvarchar](100) NOT NULL,
     [UpdatedDate] [datetime],
-    [CreatedDate] [datetime],
+    [CreatedDate] [datetime] NOT NULL,
     CONSTRAINT [PK_dbo.Roles] PRIMARY KEY ([ID])
 )
