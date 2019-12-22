5d6c16abac02710d32b2293c9f86edc2b4e5c0c6
szkantorowicz <szkantorowicz@gmail.com>
2019-12-22 22:26:06 +0100

    Skończenie Addresses

--- database_scripts/tabels/create_address.sql
+++ database_scripts/tabels/create_address.sql
@@ -1,15 +1,15 @@
-use customer_db
+use BProject_db
 
 CREATE TABLE [dbo].[Addresses] 
 (
     [ID] [int] NOT NULL IDENTITY,
     [CustomerID] [int],
-    [Street] [nvarchar](100),
-    [City] [nvarchar](100),
-    [Postcode] [nvarchar](6),
-    [Country] [nvarchar](100),
+    [Street] [nvarchar](100) NOT NULL,
+    [City] [nvarchar](100) NOT NULL,
+    [Postcode] [nvarchar](6) NOT NULL,
+    [Country] [nvarchar](100) NOT NULL,
     [Level] [int],
     [UpdatedDate] [datetime],
-    [CreatedDate] [datetime],
+    [CreatedDate] [datetime] NOT NULL,
     CONSTRAINT [PK_dbo.Addresses] PRIMARY KEY ([ID])
 )
