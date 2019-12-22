778d4b56161a425cef7f0cf1ad607075ac9a0f88
szkantorowicz <szkantorowicz@gmail.com>
2019-12-22 22:29:17 +0100

    Skończenie Category

--- database_scripts/tabels/create_category.sql
+++ database_scripts/tabels/create_category.sql
@@ -1,9 +1,9 @@
-use customer_db
+use BProject_db
 
 CREATE TABLE [dbo].[Categories] 
 (
     [ID] [int] NOT NULL IDENTITY,
-    [Name] [nvarchar](max),
-    [Description] [nvarchar](max),
+    [Name] [nvarchar] (100) NOT NULL,
+    [Description] [nvarchar](1000),
     CONSTRAINT [PK_dbo.Categories] PRIMARY KEY ([ID])
 )
