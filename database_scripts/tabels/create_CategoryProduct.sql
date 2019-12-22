cc9d73ed28f1b96f08cd2ee9f7c858fed60e4926
szkantorowicz <szkantorowicz@gmail.com>
2019-12-22 22:31:25 +0100

    Skończenie CategoryProduct

--- database_scripts/tabels/create_CategoryProduct.sql
+++ database_scripts/tabels/create_CategoryProduct.sql
@@ -1,4 +1,4 @@
-use customer_db
+use BProject_db
 
 
 CREATE TABLE [dbo].[CategoryProducts] 
@@ -7,5 +7,3 @@ CREATE TABLE [dbo].[CategoryProducts]
     [Product_ID] [int] NOT NULL,
     CONSTRAINT [PK_dbo.CategoryProducts] PRIMARY KEY ([Category_ID], [Product_ID])
 )
-CREATE INDEX [IX_Category_ID] ON [dbo].[CategoryProducts]([Category_ID])
-CREATE INDEX [IX_Product_ID] ON [dbo].[CategoryProducts]([Product_ID])
