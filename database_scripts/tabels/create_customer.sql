69d79ccbdd713f4633b5f9f7c05e0bda0e71d637
szkantorowicz <szkantorowicz@gmail.com>
2019-12-22 22:33:09 +0100

    Skończenie Customera

--- database_scripts/tabels/create_customer.sql
+++ database_scripts/tabels/create_customer.sql
@@ -1,14 +1,14 @@
-use customer_db
+use BProject_db
 
 CREATE TABLE [dbo].[Customers] (
     [ID] [int] NOT NULL IDENTITY,
     [UserID] [int],
-    [FirstName] [nvarchar](max),
-    [LastName] [nvarchar](max),
-    [UserName] [nvarchar](max),
-    [Email] [nvarchar](max),
-    [Phone] [nvarchar](max),
+    [FirstName] [nvarchar](100),
+    [LastName] [nvarchar](100),
+    [UserName] [nvarchar](100) NOT NULL,
+    [Email] [nvarchar](100) NOT NULL,
+    [Phone] [nvarchar](20) NOT NULL,
     [UpdatedDate] [datetime],
-    [CreatedDate] [datetime],
+    [CreatedDate] [datetime] NOT NULL,
     CONSTRAINT [PK_dbo.Customers] PRIMARY KEY ([ID])
 )
